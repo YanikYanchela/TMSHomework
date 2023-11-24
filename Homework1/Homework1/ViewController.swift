ecdf95d9233551a2846cb1eb2899e8ebcb5aa700
YanikYanchela <yanik-1993@rambler.ru>
2023-11-25 00:24:27 +0300

    create Xproject

---
+++ Homework1/Homework1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework1
+//
+//  Created by Дмитрий Яновский on 25.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
