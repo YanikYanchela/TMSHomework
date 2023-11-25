8ea48c0bfcff9874cf3e227166194cc72cdaf8dd
YanikYanchela <yanik-1993@rambler.ru>
2023-11-25 14:07:41 +0300

    rechenie domawki

--- Homework1/Homework1/ViewController.swift
+++ Homework1/Homework1/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var oneNumber = 4.85
+        
+        let oneNumber = 2.5
+        let twoNumber = 9.7
+        let threeNumber = 6.9
+        let fourNumber = 8.2
+
+        let summInteger = Int(oneNumber) + Int(twoNumber) + Int(threeNumber) + Int(fourNumber)
+       
+        print("сумма целых чисел = \(summInteger)")
+        
+        let summFractions: Float = Float(oneNumber - floor(oneNumber) + twoNumber - floor(twoNumber) + threeNumber - floor(threeNumber) + fourNumber - floor(fourNumber))
+        
+        print("сумма дробных частей = \(summFractions)")
     }
-
-
+    
 }
 
