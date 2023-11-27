641fcf53ef1594190d13a0efe25d7349d9b86d4c
YanikYanchela <yanik-1993@rambler.ru>
2023-11-27 17:57:51 +0300

    correct code

--- Homework1/Homework1/ViewController.swift
+++ Homework1/Homework1/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        SVProgressHUD.
         SVProgressHUD.setDefaultMaskType(.clear)
         SVProgressHUD.show(withStatus: "Loading")
         
